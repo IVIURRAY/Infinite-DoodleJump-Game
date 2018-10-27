46ccf4c37223ee2732c22ab522fb18bc2f271397
HAM <mini_muz_11@hotmail.co.uk>
2018-10-27 13:46:00 +0100

    Adding a highscore

---
+++ Assets/Score.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	public Transform player;
+	public Text highScore;
+	
+	// Update is called once per frame
+	void Update () {
+		highScore.text = "Score: " + player.position.y.ToString("0");
+	}
+}
